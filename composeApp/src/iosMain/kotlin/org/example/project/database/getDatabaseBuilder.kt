0750fadb089035d72d498075c8f3674bae6b9d96
abdo-essam <abdo.e.mosa@gmail.com>
2025-09-16 01:17:04 +0300

    fix(ios): Fix database path on iOS

--- composeApp/src/iosMain/kotlin/org/example/project/database/getDatabaseBuilder.kt
+++ composeApp/src/iosMain/kotlin/org/example/project/database/getDatabaseBuilder.kt
@@ -2,14 +2,11 @@ package org.example.project.database
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import kotlinx.cinterop.ExperimentalForeignApi
 import org.example.project.Res
-import platform.Foundation.*
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<QuranDatabase> {
-    Res.getUri("files/hafs_smart_v8.db")
-    val resourceD = Res.getUri("files/hafs_smart_v8.db").removePrefix("file://")
     return Room.databaseBuilder<QuranDatabase>(
-        name = resourceD ,
+        name = Res.getUri("files/hafs_smart_v8.db")
+            .removePrefix("file://"),
     )
 }
