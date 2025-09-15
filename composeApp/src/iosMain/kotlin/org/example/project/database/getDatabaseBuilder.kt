87ffc4edf426314e770eeb6b12008816790ff3b3
abdo-essam <abdo.e.mosa@gmail.com>
2025-09-16 01:15:29 +0300

    fix(ios): Use resource URI for database path

--- composeApp/src/iosMain/kotlin/org/example/project/database/getDatabaseBuilder.kt
+++ composeApp/src/iosMain/kotlin/org/example/project/database/getDatabaseBuilder.kt
@@ -3,37 +3,13 @@ package org.example.project.database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import kotlinx.cinterop.ExperimentalForeignApi
+import org.example.project.Res
 import platform.Foundation.*
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<QuranDatabase> {
-    val dbFilePath = documentDirectory() + "/hafs_smart_v8.db"
-    ensureBundledDatabaseCopied(dbFilePath)
+    Res.getUri("files/hafs_smart_v8.db")
+    val resourceD = Res.getUri("files/hafs_smart_v8.db").removePrefix("file://")
     return Room.databaseBuilder<QuranDatabase>(
-        name = dbFilePath,
+        name = resourceD ,
     )
-}
-
-@OptIn(ExperimentalForeignApi::class)
-private fun documentDirectory(): String {
-    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
-        directory = NSDocumentDirectory,
-        inDomain = NSUserDomainMask,
-        appropriateForURL = null,
-        create = false,
-        error = null,
-    )
-    return requireNotNull(documentDirectory?.path)
-}
-
-@OptIn(ExperimentalForeignApi::class)
-private fun ensureBundledDatabaseCopied(destinationPath: String) {
-    val fileManager = NSFileManager.defaultManager
-    if (fileManager.fileExistsAtPath(destinationPath)) return
-
-    val bundlePath = NSBundle.mainBundle.bundlePath
-    val packagedDbPath = "$bundlePath/compose-resources/composeResources/org.example.project/files/hafs_smart_v8.db"
-
-    if (fileManager.fileExistsAtPath(packagedDbPath)) {
-        fileManager.copyItemAtPath(packagedDbPath, destinationPath, error = null)
-    }
 }
